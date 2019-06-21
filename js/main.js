1e87859baa064e755d52d4750a1c44e6aae44fa2
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-06-21 15:25:06 -0400

    fixed an error with score count

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-const choices = document.querySelectorAll('choice');
+const choices = document.querySelectorAll('.choice');
 const score = document.getElementById('score');
 const result = document.getElementById('result');
 const restart = document.getElementById('restart');
 const modal = document.querySelector('.modal');
-
 const scoreboard = {
   player: 0,
   computer: 0
 };
 
-// Play Game
+// Play game
 function play(e) {
-  restart.style.display = 'inline';
+  restart.style.display = 'inline-block';
   const playerChoice = e.target.id;
   const computerChoice = getComputerChoice();
   const winner = getWinner(playerChoice, computerChoice);
   showWinner(winner, computerChoice);
 }
 
-// Get Computers Choice
+// Get computers choice
 function getComputerChoice() {
   const rand = Math.random();
   if (rand < 0.34) {
@@ -30,6 +29,7 @@ function getComputerChoice() {
   }
 }
 
+// Get game winner
 function getWinner(p, c) {
   if (p === c) {
     return 'draw';
@@ -56,61 +56,60 @@ function getWinner(p, c) {
 
 function showWinner(winner, computerChoice) {
   if (winner === 'player') {
-    // Increment player score
+    // Inc player score
     scoreboard.player++;
     // Show modal result
     result.innerHTML = `
-    <h1 class='text-win'>You Win</h1>
-    <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-    <p>NPC chose <strong>${computerChoice}</strong></p>
+      <h1 class="text-win">You Win</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>NPC Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
     `;
   } else if (winner === 'computer') {
-    // Increment computer score
+    // Inc computer score
     scoreboard.computer++;
     // Show modal result
     result.innerHTML = `
-    <h1 class='text-lose'>You Lose</h1>
-    <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-    <p>NPC chose <strong>${computerChoice}</strong></p>
+      <h1 class="text-lose">You Lose</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>NPC Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
     `;
   } else {
     result.innerHTML = `
-    <h1>It's a Draw</h1>
-    <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-    <p>NPC chose <strong>${computerChoice}</strong></p>
+      <h1>It's A Draw</h1>
+      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+      <p>NPC Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+        computerChoice.slice(1)}</strong></p>
     `;
   }
-  //   show score
+  // Show score
   score.innerHTML = `
-  <p>${scoreboard.player}</p>
-  <p>${scoreboard.computer}</p>
-  `;
+    <p>Player: ${scoreboard.player}</p>
+    <p>NPC: ${scoreboard.computer}</p>
+    `;
 
   modal.style.display = 'block';
 }
 
-// Restart Game
+// Restart game
 function restartGame() {
   scoreboard.player = 0;
   scoreboard.computer = 0;
   score.innerHTML = `
-  <p>Player = 0</p>
-  <p>Computer = 0</p>
+    <p>Player: 0</p>
+    <p>NPC: 0</p>
   `;
 }
 
-// Clear Modal
+// Clear modal
 function clearModal(e) {
   if (e.target == modal) {
     modal.style.display = 'none';
   }
 }
 
-// Event Listeners
-choices.forEach(choice => {
-  choice.addEventListener('click', play);
-});
-
+// Event listeners
+choices.forEach(choice => choice.addEventListener('click', play));
 window.addEventListener('click', clearModal);
-
 restart.addEventListener('click', restartGame);
